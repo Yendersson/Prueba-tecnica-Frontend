e1df5d71c76f80d277860b8540cd681de892c8ae
yenderson <yendersoncolmenares67@gmail.com>
2022-08-09 23:09:40 -0300

    Mejora funcionalidad de eventos

--- docs/scripts/js.js
+++ docs/scripts/js.js
@@ -1,32 +1,10 @@
-// let slideIndex = 1;
-// showSlides(slideIndex);
-
-// // THUMBNAIL IMAGE CONTROL
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
-
-// function showSlides() {
-//   let i;
-//   let slides = document.getElementsByClassName("Banner");
-//   let dots = document.getElementsByClassName("dot");
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].classList.remove('active');
-
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].classList.add("active") ;
-// }
-
 const modalMenu = document.querySelector('.modal');
-const menu = document.querySelector('.nav__menu__item');
-
-menu.addEventListener('click', ()=>{
-  modalMenu.classList.add('modal--true');
+// const menu = document.querySelector('.nav__menu__item');
+const menu = document.querySelectorAll('[data-modal]')
+// console.log(menu)
+menu.forEach(element => {
+  element.addEventListener('click', ()=>{
+    modalMenu.classList.toggle('modal--true');
+  }
+  )
 })
-
-
